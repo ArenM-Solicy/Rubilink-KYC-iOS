40de710751253ae5e4c2219323bab033080c6c55
Aren Musayelyan <aren.musayelyan@solicy.com>
2023-10-27 15:20:30 +0400

    Add Xcode Project

---
+++ Rubilink-KYC-iOS/Rubilink_KYC_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rubilink_KYC_iOSApp.swift
+//  Rubilink-KYC-iOS
+//
+//  Created by Solicy Ios on 27.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Rubilink_KYC_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
